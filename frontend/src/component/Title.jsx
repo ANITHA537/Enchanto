6c1f9f91c8aa6ee8d89f78e4e4728b033a402466
ANITHA537 <98999981+ANITHA537@users.noreply.github.com>
2025-12-16 04:01:09 +0530

    Refactor Title component styles and structure

--- frontend/src/component/Title.jsx
+++ frontend/src/component/Title.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Title({text1 ,text2}) {
+function Title({ text1, text2 }) {
   return (
-    <div className='inline-flex gap-2 items-center text-center mb-3 text-[35px] md:text-[40px]'>
-        <p className='text-blue-100'>{text1} <span className='text-[#a5faf7] '>{text2}</span></p>
-      
+    <div className='inline-flex gap-2 items-center mb-3'>
+      <p className='text-gray-500 font-serif text-3xl md:text-4xl font-medium'>{text1} <span className='text-[#B8860B] font-bold'>{text2}</span></p>
+      <p className='w-8 sm:w-12 h-[1px] sm:h-[2px] bg-[#B8860B]'></p>
     </div>
   )
 }
